ccf39b3bcf0ea1af201d3b58fbabbbe4e3402493
Brandon Alexis <brandonmachado159@gmail.com>
2022-03-31 14:46:41 -0500

    se completar rutas de usuarios y roles

--- src/routes/rolRoutes.js
+++ src/routes/rolRoutes.js
@@ -2,8 +2,16 @@ const { Router } = require('express');
 const router = Router();
 
 // importacion de controllador de roles
-const rolControllador = require('../controller/rolController');
+const rolController = require('../controller/rolController');
 
-router.get('/', rolControllador.listarRoles);
+router.get('/', rolController.listarRoles);
+
+router.get('/:id', rolController.listarRolPorId);
+
+router.post('/crear/', rolController.crearRol);
+
+router.put('/actualizar/:id', rolController.actualizarRol);
+
+router.delete('/eliminar/:id', rolController.eliminarRol);
 
 module.exports = router;
--- src/routes/userRoutes.js
+++ src/routes/userRoutes.js
@@ -5,4 +5,14 @@ const userController = require('../controller/userController');
 
 router.get('/', userController.listarUsuarios);
 
+router.get('/:id', userController.listarUsuarioPorId);
+
+router.get('/nombre/:nombre', userController.listarUsuarioPorNombre);
+
+router.post('/crear', userController.crearUsuario);
+
+router.put('/actualizar/:id', userController.actualizarUsuario);
+
+router.delete('/eliminar/:id', userController.eliminarUsuario);
+
 module.exports = router;
