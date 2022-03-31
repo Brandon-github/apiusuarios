95d3befab0939c506d86304949590144be9cfdb1
Brandon Alexis <brandonmachado159@gmail.com>
2022-03-31 10:35:55 -0500

    modificacion de controlador de usuarios para traer datos de un modelo

---
+++ src/controller/userController.js
@@ -0,0 +1,14 @@
+const controller = {};
+
+const userModel = require('../model/userModel');
+
+controller.listarUsuarios = (req, res) => {
+    userModel.obtenerUsuarios((err, rows, fields) => {
+        if (!err) 
+            res.json(rows);
+        else 
+            console.log(err);
+    })
+};
+
+module.exports = controller;
