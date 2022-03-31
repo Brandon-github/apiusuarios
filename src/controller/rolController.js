4ee92fd5c705427b740668461295e73c57ae6145
Brandon Alexis <brandonmachado159@gmail.com>
2022-03-31 14:45:36 -0500

    se completar rutas de roles y usuarios

--- src/controller/rolController.js
+++ src/controller/rolController.js
@@ -12,5 +12,49 @@ controller.listarRoles = (req, res) => {
     });
 }
 
+controller.listarRolPorId = (req, res) => {
+    const { id } = req.params;
+
+    rolModel.obtenerRolPorId([id], (err, rows, fields) => {
+        if (!err) 
+            res.json(rows);
+        else 
+            console.log(err);
+        
+    });
+}
+
+controller.crearRol = (req, res) => {
+    const { nombre } = req.body; 
+    rolModel.insertarRol([nombre], (err, rows, fields) => {
+        if (!err) 
+            res.json({Status: 'Create rol successfully'});
+        else 
+            console.log(err);
+    });
+} 
+
+controller.actualizarRol = (req, res) => {
+    const { id } = req.params;
+    const { nombre } = req.body; 
+
+    rolModel.modificarRol([nombre, id], (err, rows, fields) => {
+        if (!err) 
+            res.json({Status: 'Update rol successfully'});
+        else 
+            console.log(err);
+    });
+}
+
+controller.eliminarRol = (req, res) => {
+    const { id } = req.params;
+
+    rolModel.eliminarRol([id], (err, rows, fields) => {
+        if (!err) 
+            res.json({Status: 'Delete rol with id ' + id});
+        else
+            console.log(err);
+    });
+}
 
 module.exports = controller;
--- src/controller/userController.js
+++ src/controller/userController.js
@@ -6,10 +6,66 @@ const userModel = require('../model/userModel');
 controller.listarUsuarios = (req, res) => {
     userModel.obtenerUsuarios((err, rows, fields) => {
         if (!err) 
-            res.json(rows);
+            res.status(200).json(rows);
         else 
             console.log(err);
     })
 };
+
+controller.listarUsuarioPorId = (req, res) => {
+    const { id } = req.params;
+
+    userModel.obtenerUsuarioPorId([id], (err, rows, fields) => {
+        if (!err)   
+            res.json(rows);
+        else 
+            console.log(err);
+    });
+}
+
+controller.listarUsuarioPorNombre = (req, res) => {
+    const { nombre } = req.params;
+
+    userModel.obtenerUsuarioPorNombre([nombre], (err, rows, fields) => {
+        if (!err) 
+            res.json(rows);
+        else 
+            console.log(rows);
+    });
+} 
+
+controller.crearUsuario = (req, res) => {
+    const { nombre, correo, id_rol } = req.body;
+
+    userModel.insertarUsuario([nombre, correo, id_rol], (err, rows, fields) => {
+        if (!err) 
+            res.json({Status: 'create user successfully'});
+        else 
+            console.log(err);
+    });
+}
+
+controller.actualizarUsuario = (req, res) => {
+    const { id } = req.params;
+    const { nombre, correo, id_rol } = req.body;
+
+    userModel.modificarUsuario([nombre, correo, id_rol, id], (err, rows, fields) => {
+        if (!err)
+            res.json({Status: 'update user successfully'});
+        else 
+            console.log(err);
+    });
+}
+
+controller.eliminarUsuario = (req, res) => {
+    const { id } = req.params;
+
+    userModel.eliminarUsuario([id], (err, rows, fields) => {
+        if (!err) 
+            res.json({Status: 'Delete user with id' + id});
+        else 
+            console.log(err);
+    });
+}
 
 module.exports = controller;
