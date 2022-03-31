b3c7b4511876aa84e9d8560fbcd229641161b352
Brandon Alexis <brandonmachado159@gmail.com>
2022-03-31 10:35:21 -0500

    adicion de rutas de usuarios

--- src/server.js
+++ src/server.js
@@ -1,7 +1,13 @@
 const express = require('express');
 const app = express();
 
-// Settings
+// Importacion de rutas
+const userRoutes = require('./routes/userRoutes');
+
+// Configuraciones
 app.set('port', process.env.PORT || 5000);
+
+// Rutas
+app.use('/api/users', userRoutes);
 
 module.exports = app;
