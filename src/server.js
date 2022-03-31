6f652019eee56caa17eb81dea9df5c1de041d87c
Brandon Alexis <brandonmachado159@gmail.com>
2022-03-31 11:54:07 -0500

    importacion de rutas de roles en el servidor

--- src/server.js
+++ src/server.js
@@ -3,11 +3,13 @@ const app = express();
 
 // Importacion de rutas
 const userRoutes = require('./routes/userRoutes');
+const rolRoutes = require('./routes/rolRoutes');
 
 // Configuraciones
 app.set('port', process.env.PORT || 5000);
 
 // Rutas
 app.use('/api/users', userRoutes);
+app.use('/api/roles', rolRoutes);
 
 module.exports = app;
